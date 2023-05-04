497a4963bc7998efe743451c2d9743be1d116762
toluwase <toluwasethomas1@gmail.com>
2023-05-04 12:38:27 +0100

    config

--- config/config.go
+++ config/config.go
@@ -1,2 +1,36 @@
 package config
 
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Debug            bool   `envconfig:"debug"`
+	Port             int    `envconfig:"port"`
+	PostgresHost     string `envconfig:"postgres_host"`
+	PostgresUser     string `envconfig:"postgres_user"`
+	PostgresDB       string `envconfig:"postgres_db"`
+	BaseUrl          string `envconfig:"base_url"`
+	Env              string `envconfig:"env"`
+	PostgresPort     int    `envconfig:"postgres_port"`
+	PostgresPassword string `envconfig:"postgres_password"`
+	JWTSecret        string `envconfig:"jwt_secret"`
+}
+
+func Load() (*Config, error) {
+	env := os.Getenv("GIN_MODE")
+	if env != "release" {
+		if err := godotenv.Load("./.env"); err != nil {
+			log.Printf("couldn't load env vars: %v", err)
+		}
+	}
+
+	c := &Config{}
+	err := envconfig.Process("zeina", c)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
